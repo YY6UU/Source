0f5caa21ac0be5e706ae621aa72dde01aecdbc77
YY6UU <146486005+YY6UU@users.noreply.github.com>
2023-11-05 18:07:03 +0300

    update functions.py

--- zthon/helpers/functions/functions.py
+++ zthon/helpers/functions/functions.py
@@ -98,8 +98,8 @@ async def covidindia(state):
 async def post_to_telegraph(
     page_title,
     html_format_content,
-    auth_name="Zed-Thon",
-    auth_url="https://t.me/ZedThon",
+    auth_name="Tepthon",
+    auth_url="https://t.me/Tepthon",
 ):
     post_client = TelegraphPoster(use_api=True)
     post_client.create_api_token(auth_name)
